e47d58fd8a222fa32b8babe3d1750379682d1cab
CurtisNewbie <zhuangyongj@gmail.com>
2021-07-19 23:18:04 +0800

    Configure dubbo

--- auth-service/src/main/java/com/curtisnewbie/AuthServiceApplication.java
+++ auth-service/src/main/java/com/curtisnewbie/AuthServiceApplication.java
@@ -1,13 +1,17 @@
 package com.curtisnewbie;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource(value = "classpath:dubbo.properties")
+@EnableDubbo
 @SpringBootApplication
 public class AuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
 
 }
