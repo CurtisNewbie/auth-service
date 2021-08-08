f747f27efddb24b6d3e212c1e0e5821ccd1d3458
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-08 21:09:48 +0800

    Fix test case

--- auth-service/src/test/java/com/curtisnewbie/service/auth/local/api/TestLocalOperateLogService.java
+++ auth-service/src/test/java/com/curtisnewbie/service/auth/local/api/TestLocalOperateLogService.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
@@ -15,6 +16,7 @@ import java.util.Date;
  *
  * @author yongjie.zhuang
  */
+@Transactional
 @SpringBootTest
 @Rollback
 public class TestLocalOperateLogService {
