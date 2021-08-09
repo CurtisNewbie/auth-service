21674494c3cac965bf763002e5b6c294ba2c59da
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-09 23:55:10 +0800

    Update test build script

--- auth-service/test-build.sh
+++ auth-service/test-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-mvn clean package 
+mvn clean package
 
-scp "target/auth-service-0.0.1.jar" "zhuangyongj@192.168.10.128:~/exec/authserver.jar"
+ssh -l zhuangyongj 192.168.10.128 "/home/zhuangyongj/exec/authserver/kill-as.sh"
+
+scp "target/auth-service-0.0.1.jar" "zhuangyongj@192.168.10.128:~/exec/authserver/authserver.jar"
