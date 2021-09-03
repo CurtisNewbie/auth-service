8bc5f522aac9343d4b98c6a362da06529d6924a6
CurtisNewbie <zhuangyongj@gmail.com>
2021-09-03 18:15:48 +0800

    Fix redis config for distributed task module

--- auth-service/src/main/java/com/curtisnewbie/AuthServiceApplication.java
+++ auth-service/src/main/java/com/curtisnewbie/AuthServiceApplication.java
@@ -2,10 +2,17 @@ package com.curtisnewbie;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+
+import java.io.IOException;
 
 @MapperScan("com.curtisnewbie.service.auth.dao")
 @PropertySources({
@@ -19,6 +26,17 @@ public class AuthServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+
+    @Value("${redisson-config}")
+    private String redissonConfig;
+
+    @Bean
+    public RedissonClient redissonClient() throws IOException {
+        Config config = Config.fromYAML(this.getClass().getClassLoader().getResourceAsStream(redissonConfig));
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
     }
 
 }
