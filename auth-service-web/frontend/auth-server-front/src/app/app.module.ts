1659fc1b2e1eed0e18023e654d0c25f7fd951fb4
CurtisNewbie <zhuangyongj@gmail.com>
2021-09-08 19:36:33 +0800

    Use bootstrap's btn-close for btn that clears input

--- auth-service-web/frontend/auth-server-front/src/app/app.module.ts
+++ auth-service-web/frontend/auth-server-front/src/app/app.module.ts
@@ -24,7 +24,6 @@ import { MatTableModule } from "@angular/material/table";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatSelectModule } from "@angular/material/select";
 import { MatDatepickerModule } from "@angular/material/datepicker";
@@ -73,7 +72,6 @@ import { ScrollingModule } from "@angular/cdk/scrolling";
     MatTooltipModule,
     MatPaginatorModule,
     MatButtonModule,
-    MatIconModule,
     MatInputModule,
     MatSelectModule,
     MatDialogModule,
