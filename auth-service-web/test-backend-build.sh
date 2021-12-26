ff6f25907270a5d49291ce98f632e76a88e32471
CurtisNewbie <zhuangyongj@gmail.com>
2021-12-26 17:10:11 +0800

    Fix scripts

--- auth-service-web/test-backend-build.sh
+++ auth-service-web/test-backend-build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#remoteaddr="192.168.10.128"
-remoteaddr="192.168.31.103"
-jarname="auth-service-web-1.0.2.jar"
+jarname="auth-service-web-1.0.3-SNAPSHOT.jar"
 
 mvn clean package -Dmaven.test.skip=true
 
@@ -12,4 +10,4 @@ then
     exit -1
 fi
 
-scp "target/${jarname}" "zhuangyongj@${remoteaddr}:~/services/auth-service-web/auth-service-web.jar"
+scp "target/${jarname}" "zhuangyongj@curtisnewbie.com:~/services/auth-service-web/auth-service-web.jar"
--- auth-service/test-build.sh
+++ auth-service/test-build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-jarname="auth-service-1.0.2.jar"
+jarname="auth-service-1.0.3-SNAPSHOT.jar"
 
 mvn clean package -Dmaven.test.skip=true
-
-# ssh -l zhuangyongj 192.168.10.128 "/home/zhuangyongj/exec/authserver/kill-as.sh"
 
 scp "target/${jarname}" "zhuangyongj@curtisnewbie.com:~/services/auth-service/auth-service.jar"
