c309d1144aef839444f71bc650c237131263a005
yongj.zhuang <zhuangyongj@gmail.com>
2022-05-01 20:45:14 +0800

    Remove redis session

--- auth-service/src/main/java/com/curtisnewbie/AuthServiceApplication.java
+++ auth-service/src/main/java/com/curtisnewbie/AuthServiceApplication.java
@@ -9,14 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import java.io.IOException;
 
 @MapperScan("com.curtisnewbie.service.auth.infrastructure.repository.mapper")
 @EnableDiscoveryClient
 @SpringBootApplication
-@EnableRedisHttpSession(redisNamespace = "auth-service:session") // todo remove this
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
