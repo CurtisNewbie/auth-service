5cf1a74f30883e8e33126f3445b806949b2ec5ef
yongj.zhuang <zhuangyongj@gmail.com>
2022-05-14 23:38:51 +0800

    sql scripts

--- auth-service/src/main/resources/scripts/1.1.0.sql
+++ auth-service/src/main/resources/scripts/1.1.0.sql
@@ -1,10 +1,19 @@
 alter table access_log_history add column token VARCHAR(1000) DEFAULT '' COMMENT 'token' after url;
 alter table access_log add column token VARCHAR(1000) DEFAULT '' COMMENT 'token' after url;
+
+-- add review_status, for the existing records, we set them to "APPROVED"
 alter table user add column review_status VARCHAR(25) NOT NULL COMMENT 'Review Status' after is_disabled;
 update user set review_status = "APPROVED" where is_del = 0;
+
+-- drop event_handling table, it's deprecated
 drop table event_handling if exists;
 
 -- before generating the user_no using a task or filling them manually
 alter table user add column user_no VARCHAR(64) NOT NULL DEFAULT '' COMMENT 'user no';
--- after generating the user_no for existing records, add required constraints
+
+-- add the new job to generate user_no, then trigger job
+INSERT INTO task (job_name,target_bean,cron_expr,app_group,last_run_start_time,last_run_end_time,last_run_by,enabled,concurrent_enabled,last_run_result,update_date,update_by)
+	VALUES ('GenerateUserNoJob','generateUserNoJob','0 0 0 ? * *','auth-service',now(),now(),'',1,0,'',now(),'');
+
+-- after generating the user_no for existing records, add required constraints back to it
 alter table user modify column user_no VARCHAR(64) NOT NULL UNIQUE COMMENT 'user no';
--- auth-service/src/main/resources/scripts/script.sql
+++ auth-service/src/main/resources/scripts/script.sql
@@ -99,3 +99,32 @@ CREATE TABLE IF NOT EXISTS operate_log_history (
     username VARCHAR(255) NOT NULL COMMENT 'username',
     user_id INT UNSIGNED NOT NULL COMMENT 'primary key of user'
 ) ENGINE=InnoDB COMMENT "operate log history (for history only)";
+
+-- copied from distributed-task-module
+CREATE TABLE task (
+    id INT PRIMARY KEY AUTO_INCREMENT COMMENT "id",
+    job_name VARCHAR(255) NOT NULL COMMENT "job's name",
+    target_bean VARCHAR(255) NOT NULL COMMENT "name of bean that will be executed",
+    cron_expr VARCHAR(255) NOT NULL COMMENT "cron expression",
+    app_group VARCHAR(255) NOT NULL COMMENT "app group that runs this task",
+    last_run_start_time TIMESTAMP COMMENT "the last time this task was executed",
+    last_run_end_time TIMESTAMP COMMENT "the last time this task was finished",
+    last_run_by VARCHAR(255) COMMENT "task triggered by",
+    last_run_result VARCHAR(255) COMMENT "result of last execution",
+    enabled INT NOT NULL DEFAULT 0 COMMENT "whether the task is enabled: 0-disabled, 1-enabled",
+    concurrent_enabled INT NULL DEFAULT 0 COMMENT "whether the task can be executed concurrently: 0-disabled, 1-enabled",
+    update_date TIMESTAMP COMMENT 'update time',
+    update_by VARCHAR(255) COMMENT 'updated by'
+) ENGINE=InnoDB COMMENT "task";
+
+-- copied from distributed-task-module
+CREATE TABLE task_history (
+    id INT PRIMARY KEY AUTO_INCREMENT COMMENT "id",
+    task_id INT COMMENT "task id",
+    start_time TIMESTAMP COMMENT "start time",
+    end_time TIMESTAMP COMMENT "end time",
+    run_by VARCHAR(255) COMMENT "task triggered by",
+    run_result VARCHAR(255) COMMENT "result of last execution",
+    create_time DATETIME COMMENT "create time"
+) ENGINE=InnoDB COMMENT "task history";
+
