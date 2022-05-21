fa7731920a6f79541e085173c8f4f3caba743419
yongj.zhuang <zhuangyongj@gmail.com>
2022-05-21 13:52:46 +0800

    Update deploy.sh

--- auth-service/deploy.sh
+++ auth-service/deploy.sh
@@ -4,4 +4,4 @@ jarname="auth-service-build.jar"
 
 mvn clean package -Dmaven.test.skip=true
 
-scp "target/${jarname}" "zhuangyongj@curtisnewbie.com:~/services/auth-service/auth-service.jar"
+scp "target/${jarname}" "zhuangyongj@curtisnewbie.com:~/services/auth-service/build/auth-service.jar"
