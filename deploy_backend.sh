612700e11674d5d9e4236a72543267644c71bb9f
yongj.zhuang <zhuangyongj@gmail.com>
2022-09-16 23:43:54 +0800

    Modified:  auth-service/src/main/resources/bootstrap-test.yml Modified:  deploy_backend.sh

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -7,9 +7,22 @@ jarname="auth-service-build.jar"
 
 # dependencies
 mvn install -Dmaven.test.skip=true
+if [ ! $? -eq 0 ]; then
+    exit -1
+fi
+
 
 # packaging
 mvn package -f auth-service/pom.xml -Dmaven.test.skip=true
+if [ ! $? -eq 0 ]; then
+    exit -1
+fi
+
 
 # copy to remove server
 scp "auth-service/target/${jarname}" "${remote}:${remote_path}"
+if [ ! $? -eq 0 ]; then
+    exit -1
+fi
+
+ssh  "alphaboi@curtisnewbie.com" "cd services; docker-compose up -d --build auth-service"
