9ae6e6eb40cfefdfe9c0bd34cbc121f3592971c0
yongj.zhuang <zhuangyongj@gmail.com>
2022-10-02 23:05:10 +0800

    Modified:  auth-service/src/main/resources/scripts/script.sql

--- auth-service/src/main/resources/scripts/script.sql
+++ auth-service/src/main/resources/scripts/script.sql
@@ -1,94 +1,106 @@
+CREATE TABLE `user` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `username` varchar(255) NOT NULL COMMENT 'username (must be unique)',
+  `password` varchar(255) NOT NULL COMMENT 'password in hash',
+  `salt` varchar(10) NOT NULL COMMENT 'salt',
+  `role` varchar(20) NOT NULL COMMENT 'role',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'when the record is created',
+  `create_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
+  `is_disabled` int NOT NULL DEFAULT '0' COMMENT 'whether the user is disabled, 0-normal, 1-disabled',
+  `review_status` varchar(25) NOT NULL COMMENT 'Review Status',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'when the record is updated',
+  `update_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
+  `is_del` tinyint NOT NULL DEFAULT '0' COMMENT '0-normal, 1-deleted',
+  `user_no` varchar(64) NOT NULL COMMENT 'user no',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `username_2` (`username`),
+  UNIQUE KEY `user_no` (`user_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS user (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT "primary key",
-    user_no VARCHAR(64) NOT NULL UNIQUE COMMENT 'user no',
-    username VARCHAR(255) NOT NULL UNIQUE COMMENT "username (must be unique)",
-    password VARCHAR(255) NOT NULL COMMENT "password in hash",
-    salt VARCHAR(10) NOT NULL COMMENT "salt",
-    role VARCHAR(20) NOT NULL COMMENT "role",
-    is_disabled INT NOT NULL DEFAULT 0 COMMENT 'whether the user is disabled, 0-normal, 1-disabled',
-    review_status VARCHAR(25) NOT NULL COMMENT 'Review Status',
-    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'when the record is created',
-    create_by VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
-    update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'when the record is updated',
-    update_by VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
-    is_del TINYINT NOT NULL DEFAULT '0' COMMENT '0-normal, 1-deleted'
-) ENGINE=InnoDB COMMENT 'user';
+CREATE TABLE `user_key` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `user_id` int unsigned NOT NULL COMMENT 'user.id',
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT 'name of the key',
+  `secret_key` varchar(255) NOT NULL COMMENT 'secret key',
+  `expiration_time` datetime NOT NULL COMMENT 'when the key is expired',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'when the record is created',
+  `create_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'when the record is updated',
+  `update_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
+  `is_del` tinyint NOT NULL DEFAULT '0' COMMENT '0-normal, 1-deleted',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `secret_key` (`secret_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT="user's key";
 
-CREATE table if not exists user_key (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT "primary key",
-    user_id INT UNSIGNED NOT NULL COMMENT "user.id",
-    name VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'name of the key',
-    secret_key VARCHAR(255) NOT NULL UNIQUE COMMENT "secret key",
-    expiration_time DATETIME NOT NULL COMMENT 'when the key is expired',
-    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'when the record is created',
-    create_by VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
-    update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'when the record is updated',
-    update_by VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
-    is_del TINYINT NOT NULL DEFAULT '0' COMMENT '0-normal, 1-deleted'
-) ENGINE=InnoDB COMMENT "user's key";
+CREATE TABLE `app` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `name` varchar(255) NOT NULL COMMENT 'name of the application',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'when the record is created',
+  `create_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'when the record is updated',
+  `update_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
+  `is_del` tinyint NOT NULL DEFAULT '0' COMMENT '0-normal, 1-deleted',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='application';
 
-CREATE TABLE IF NOT EXISTS app (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT "primary key",
-    name VARCHAR (255) UNIQUE NOT NULL COMMENT 'name of the application',
-    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'when the record is created',
-    create_by VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
-    update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'when the record is updated',
-    update_by VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
-    is_del TINYINT NOT NULL DEFAULT '0' COMMENT '0-normal, 1-deleted'
-) ENGINE=InnoDB COMMENT 'application';
+CREATE TABLE `user_app` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `user_id` int unsigned NOT NULL COMMENT 'user''s id',
+  `app_id` int unsigned NOT NULL COMMENT 'app''s id',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'when the record is created',
+  `create_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'when the record is updated',
+  `update_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
+  `is_del` tinyint NOT NULL DEFAULT '0' COMMENT '0-normal, 1-deleted',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_user_app` (`user_id`,`app_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='join table between application and user';
 
-CREATE TABLE IF NOT EXISTS user_app (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT "primary key",
-    user_id INT UNSIGNED NOT NULL COMMENT "user's id",
-    app_id INT UNSIGNED NOT NULL COMMENT "app's id",
-    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'when the record is created',
-    create_by VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
-    update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'when the record is updated',
-    update_by VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
-    is_del TINYINT NOT NULL DEFAULT '0' COMMENT '0-normal, 1-deleted',
-    CONSTRAINT uk_user_app UNIQUE (user_id, app_id)
-) ENGINE=InnoDB COMMENT 'join table between application and user';
+CREATE TABLE `access_log` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `access_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'when the user signed in',
+  `ip_address` varchar(255) NOT NULL COMMENT 'ip address',
+  `username` varchar(255) NOT NULL COMMENT 'username',
+  `user_id` int unsigned NOT NULL COMMENT 'primary key of user',
+  `url` varchar(255) DEFAULT '' COMMENT 'request url',
+  `token` varchar(1000) DEFAULT '' COMMENT 'token',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='access log';
 
-CREATE TABLE IF NOT EXISTS access_log (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT 'primary key',
-    access_time TIMESTAMP NOT NULL DEFAULT NOW() COMMENT 'when the user signed in',
-    ip_address VARCHAR(255) NOT NULL COMMENT 'ip address',
-    username VARCHAR(255) NOT NULL COMMENT 'username',
-    user_id INT UNSIGNED NOT NULL COMMENT 'primary key of user',
-    url VARCHAR(255) DEFAULT '' COMMENT 'request url',
-    token VARCHAR(1000) DEFAULT '' COMMENT 'token'
-) ENGINE=InnoDB COMMENT 'access log';
+CREATE TABLE `access_log_history` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `access_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'when the user signed in',
+  `ip_address` varchar(255) NOT NULL COMMENT 'ip address',
+  `username` varchar(255) NOT NULL COMMENT 'username',
+  `user_id` int unsigned NOT NULL COMMENT 'primary key of user',
+  `url` varchar(255) DEFAULT '' COMMENT 'request url',
+  `token` varchar(1000) DEFAULT '' COMMENT 'token',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='access log (for history only)';
 
-CREATE TABLE IF NOT EXISTS access_log_history (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT 'primary key',
-    access_time TIMESTAMP NOT NULL DEFAULT NOW() COMMENT 'when the user signed in',
-    ip_address VARCHAR(255) NOT NULL COMMENT 'ip address',
-    username VARCHAR(255) NOT NULL COMMENT 'username',
-    user_id INT UNSIGNED NOT NULL COMMENT 'primary key of user',
-    url VARCHAR(255) DEFAULT '' COMMENT 'request url',
-    token VARCHAR(1000) DEFAULT '' COMMENT 'token'
-) ENGINE=InnoDB COMMENT 'access log (for history only)';
+CREATE TABLE `operate_log` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `operate_name` varchar(255) NOT NULL COMMENT 'name of operation',
+  `operate_desc` varchar(255) NOT NULL DEFAULT '' COMMENT 'description of operation',
+  `operate_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'when the operation happens',
+  `operate_param` varchar(1000) NOT NULL COMMENT 'parameters used for the operation',
+  `username` varchar(255) NOT NULL COMMENT 'username',
+  `user_id` int unsigned NOT NULL COMMENT 'primary key of user',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='operate log';
 
-CREATE TABLE IF NOT EXISTS operate_log (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT 'primary key',
-    operate_name VARCHAR(255) NOT NULL COMMENT 'name of operation',
-    operate_desc VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'description of operation',
-    operate_time TIMESTAMP NOT NULL DEFAULT NOW() COMMENT 'when the operation happens',
-    operate_param VARCHAR(1000) NOT NULL  COMMENT 'parameters used for the operation',
-    username VARCHAR(255) NOT NULL COMMENT 'username',
-    user_id INT UNSIGNED NOT NULL COMMENT 'primary key of user'
-) ENGINE=InnoDB COMMENT "operate log";
-
-CREATE TABLE IF NOT EXISTS operate_log_history (
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT COMMENT 'primary key',
-    operate_name VARCHAR(255) NOT NULL COMMENT 'name of operation',
-    operate_desc VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'description of operation',
-    operate_time TIMESTAMP NOT NULL DEFAULT NOW() COMMENT 'when the operation happens',
-    operate_param VARCHAR(1000) NOT NULL  COMMENT 'parameters used for the operation',
-    username VARCHAR(255) NOT NULL COMMENT 'username',
-    user_id INT UNSIGNED NOT NULL COMMENT 'primary key of user'
-) ENGINE=InnoDB COMMENT "operate log history (for history only)";
+CREATE TABLE `operate_log_history` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `operate_name` varchar(255) NOT NULL COMMENT 'name of operation',
+  `operate_desc` varchar(255) NOT NULL DEFAULT '' COMMENT 'description of operation',
+  `operate_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'when the operation happens',
+  `operate_param` varchar(1000) NOT NULL COMMENT 'parameters used for the operation',
+  `username` varchar(255) NOT NULL COMMENT 'username',
+  `user_id` int unsigned NOT NULL COMMENT 'primary key of user',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='operate log history (for history only)';
 
 -- copied from distributed-task-module
 CREATE TABLE task (
@@ -104,7 +116,8 @@ CREATE TABLE task (
     enabled INT NOT NULL DEFAULT 0 COMMENT "whether the task is enabled: 0-disabled, 1-enabled",
     concurrent_enabled INT NULL DEFAULT 0 COMMENT "whether the task can be executed concurrently: 0-disabled, 1-enabled",
     update_date TIMESTAMP COMMENT 'update time',
-    update_by VARCHAR(255) COMMENT 'updated by'
+    update_by VARCHAR(255) COMMENT 'updated by',
+    KEY app_group_idx (app_group)
 ) ENGINE=InnoDB COMMENT "task";
 
 -- copied from distributed-task-module
@@ -115,6 +128,13 @@ CREATE TABLE task_history (
     end_time TIMESTAMP COMMENT "end time",
     run_by VARCHAR(255) COMMENT "task triggered by",
     run_result VARCHAR(255) COMMENT "result of last execution",
-    create_time DATETIME COMMENT "create time"
+    create_time DATETIME COMMENT "create time",
+    KEY task_id_idx (task_id)
 ) ENGINE=InnoDB COMMENT "task history";
+
+-- tasks used by this service
+INSERT INTO `task` (`job_name`, `target_bean`, `cron_expr`, `app_group`, `last_run_start_time`, `last_run_end_time`, `last_run_by`, `last_run_result`, `enabled`, `concurrent_enabled`, `update_date`, `update_by`)
+VALUES ('AccessLogHistoryJob','moveAccessLogHistoryJob','0 0 /6 ? * *','auth-service',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'','',1,0,CURRENT_TIMESTAMP,''),
+('OperateLogHistoryJob','moveOperateLogHistoryJob','0 0 /6 ? * *','auth-service',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'','',0,0,CURRENT_TIMESTAMP,''),
+('GenerateUserNoJob','generateUserNoJob','0 0 0 ? * *','auth-service',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,'','',0,0,CURRENT_TIMESTAMP,'');
 
